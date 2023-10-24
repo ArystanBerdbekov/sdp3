e966d8b5398ae4014abfdc4a20f9c83118920e8c
ArystanBerdbekov <147814038+ArystanBerdbekov@users.noreply.github.com>
2023-10-24 15:01:26 +0600

    Delete src/SecondAction.java

--- src/SecondAction.java
+++
@@ -1,8 +0,0 @@
-public class SecondAction {
-    public void saveObject(){
-        System.out.println("Saving...");
-    }
-    public void deleteObject(){
-        System.out.println("Deleting...");
-    }
-}
